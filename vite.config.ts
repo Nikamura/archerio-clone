5b4485b28ac9d4fbb950f56989ef9a411c022bd0
Karolis Mazukna <k@karolis.dev>
2026-01-01 15:29:23 +0200

    Update Vite configuration for GitHub Pages deployment and modify CI workflow
    - Set base path for Vite app to '/archerio-clone/' for proper deployment.
    - Refactor GitHub Actions workflow to include build and deploy jobs, ensuring a streamlined process for building and deploying the Vite app to GitHub Pages.
    - Remove unused enemy sprite assets and add a new portal sprite asset.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  base: '/archerio-clone/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
